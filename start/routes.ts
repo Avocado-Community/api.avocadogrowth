91bca7552ec9970b6fcab3e1adfff4a5885f62c8
Moumouuu <robin.travail007@gmail.com>
2024-04-15 12:44:13 +0200

    delete unused code

--- start/routes.ts
+++ start/routes.ts
@@ -7,16 +7,9 @@
 |
 */
 
-import { middleware } from '#start/kernel'
 import router from '@adonisjs/core/services/router'
 
 // controller imports
 const SessionController = () => import('#controllers/session_controller')
 
 router.post('/register', [SessionController, 'store'])
-
-router
-  .get('/', async () => {
-    return { hello: 'world' }
-  })
-  .use(middleware.auth())
