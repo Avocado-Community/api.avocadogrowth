64d0549907134f143f10f54d2dfac34e9dc6a38d
Moumouuu <robin.travail007@gmail.com>
2024-04-17 10:58:40 +0200

    test: :test_tube: write test for login

--- app/features/auth/validators/session_validator.ts
+++ app/features/auth/validators/session_validator.ts
@@ -20,3 +20,22 @@ vine.messagesProvider = new SimpleMessagesProvider({
 
   'password.minLength': 'The password must be at least 8 characters long',
 })
+
+/**
+ * Validates the user's login action
+ */
+export const showSessionValidator = vine.compile(
+  vine.object({
+    email: vine.string().email().trim(),
+    password: vine.string().minLength(8),
+  })
+)
+
+vine.messagesProvider = new SimpleMessagesProvider({
+  // Applicable for all fields
+  'required': 'The {{ field }} field is required',
+  'string': 'The value of {{ field }} field must be a string',
+  'email': 'The value is not a valid email address',
+
+  'password.minLength': 'The password must be at least 8 characters long',
+})
